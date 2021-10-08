8f9faf0471f869014d625b4b25fb631ca5b1e9dd
ashwani1218 <ashwanipandey1218@gmail.com>
2021-10-08 16:51:35 +0530

    Updated Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Spring Graphql Example
+This repository contains example for implementing graphql with spring boot.
+
+### Endpoints
+
+#### Launches a Graphiql Ui to interact with the api.
+```
+http://localhost:8080/graphiql
+```
+
+### Available Scripts
+
+#### Create jar file
+```
+$ mvn clean install
+```
+
+
+#### Run application
+```
+$ mvn spring-boot:run
+
+OR
+
+$ java -jar graphql-0.0.1-SNAPSHOT
+```
+
--- src/main/java/com/ashwani/graphql/entity/Post.java
+++ src/main/java/com/ashwani/graphql/entity/Post.java
@@ -4,6 +4,6 @@ package com.ashwani.graphql.entity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.annotation.Id;
 
-public record Post(@JsonProperty("id")@Id Long id, @JsonProperty("title") String title
+public record Post(@JsonProperty("id") @Id Long id, @JsonProperty("title") String title
         , @JsonProperty("category") String category, @JsonProperty("authorId") String authorId) {
 }
